8484dc6463f7542c4c99b75468f95784904d5504
Petr <petiauzunov1997481@gmail.com>
2022-09-28 15:39:11 +0400

    Второе задание

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        var dogs = 8.0;
+        System.out.println(dogs);
+        dogs = dogs - 3.5;
+        System.out.println(dogs);
+        var cats = 3.6;
+        System.out.println(cats);
+        cats = cats - 1.6;
+        System.out.println(cats);
+        var paper = 763789;
+        System.out.println(paper);
+        paper = paper - 7639;
+        System.out.println(paper);
+    }
+}
