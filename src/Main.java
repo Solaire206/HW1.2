e321b7bb3fc2194543955c6b5799818eeca9db0d
Petr <petiauzunov1997481@gmail.com>
2022-09-28 16:22:04 +0400

    задание 1-3

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
 public class Main {
     public static void main(String[] args) {
+        //Задача 1
+        System.out.println("Задача №1");
         var dogs = 8.0;
-        System.out.println(dogs);
-        dogs = dogs - 3.5;
         System.out.println(dogs);
         var cats = 3.6;
         System.out.println(cats);
-        cats = cats - 1.6;
-        System.out.println(cats);
         var paper = 763789;
         System.out.println(paper);
+        //Задача 2
+        System.out.println("Задача №2");
+        dogs = dogs + 4;
+        System.out.println(dogs);
+        cats = cats + 4;
+        System.out.println(cats);
+        paper = paper + 4;
+        System.out.println(paper);
+        //Задача 3
+        System.out.println("Задача №3");
+        dogs = dogs - 3.5;
+        System.out.println(dogs);
+        cats = cats - 1.6;
+        System.out.println(cats);
         paper = paper - 7639;
         System.out.println(paper);
     }
